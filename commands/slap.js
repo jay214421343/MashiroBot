bb707ec05e27e241d59855d8e047f91fbbe6974a
jay214421343 <48416316+jay214421343@users.noreply.github.com>
2019-06-12 17:31:59 +0200

    Update slap.js

--- commands/slap.js
+++ commands/slap.js
@@ -1,3 +1,5 @@
+const Discord = require("discord.js");
+
 exports.run = (bot, message, args, tools) => {
 
     var images = ["https://i.imgur.com/fm49srQ.gif", "https://i.imgur.com/o2SJYUS.gif", "https://i.imgur.com/Agwwaj6.gif", "https://i.gifer.com/cCX.gif", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7vgxCdD_ojfyfBGdZPeei11Hm7cK3zrPJTcsTYZ9Tl-dsA7upzw", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUPZp5heqxYIWj0l1G0KDC3o9YqJvmAG2FqP6n1BSVGwO5pnSMgg", "https://pa1.narvii.com/6067/68a2bb5829d993b3362a37dce55caa9c0949af6c_hq.gif", ];
