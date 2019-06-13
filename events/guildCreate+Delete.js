f3b2f6c7d6af1da9629abb43c59d9c4f592ea3b8
jay214421343 <48416316+jay214421343@users.noreply.github.com>
2019-06-13 17:20:46 +0200

    Create guildCreate+Delete.js
    Xd New update

---
+++ events/guildCreate+Delete.js
@@ -0,0 +1,9 @@
+const bot = require('../bot.js')
+const request = require('request');
+const auth = require('../config')
+
+
+bot.on('guildCreate', (g) => {
+    let channels = g.channels
+    let defaultChannel = channels.first()
+    if (defaultChannel && defaultChannel.send) defaultChannel.send('**Hello! I am MultiBot, your helpful server management and community assistant Discord:tm: Bot**\n**You can get started `/help`\n`\n**The permission system uses Discords permissions,*\n\n**Links and Support**\nOfficial Server: <https://discord.gg/eVy4Qjd>')
