ee1246b1e174ff732c5531db493df1b8d63782dd
jay214421343 <48416316+jay214421343@users.noreply.github.com>
2019-06-21 15:54:40 +0200

    Create contact.js

---
+++ commands/contact.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+
+module.exports.run = (bot, message, args, discord) => {
+    var mes = args.join(' ')
+    let embed = new discord.RichEmbed()
+    .setTitle(`Contact System`)
+    .setDescription(mes)
+    .setColor(`BLUE`)
+    .setFooter(`This message was sent by ${message.author.tag}`)
+    .setThumbnail(bot.user.avatarURL)
+    bot.channels.find('id','591142462123147279').send({ embed })
+}
