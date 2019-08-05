ad84a2818590d86307993f07d5ebe9aa1cba3687
jay214421343 <48416316+jay214421343@users.noreply.github.com>
2019-08-05 15:50:06 +0200

    Create premium.js

---
+++ commands/premium.js
@@ -0,0 +1,14 @@
+const premiumServers = require("../config.json").premiumServers
+const checkPremium = require('../util/checkPremium.js');
+const Discord = require('discord.js');
+
+module.exports.run = (bot, message, args) => {
+  let preMessage = checkPremium(bot, message, true)
+
+  let em = new Discord.RichEmbed()
+    .setColor("7289DA")
+    .setAuthor(`${bot.user.username} Premium`, bot.user.avatarURL)
+    .setDescription(preMessage)
+
+  message.channel.send({ embed: em }).then(m => m.delete(55000))
+}
